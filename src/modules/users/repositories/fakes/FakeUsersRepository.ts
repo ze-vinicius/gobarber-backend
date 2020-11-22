9ab004aafb0abd5cb0f9dba1cc61770dc5bc31e5
Vinicius Barcelos <josevsaraiva@gmail.com>
2020-11-22 15:42:51 -0300

    test: adicionando os testes de criação e autenticação de usuário

---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,51 @@
+import { v4 } from 'uuid';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+class UsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  public async findById(id: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.id === id);
+
+    return findUser;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.email === email);
+
+    return findUser;
+  }
+
+  public async create({
+    name,
+    email,
+    password,
+  }: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, {
+      id: v4(),
+      name,
+      email,
+      password,
+    });
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
+  }
+}
+
+export default UsersRepository;
