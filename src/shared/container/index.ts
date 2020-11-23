6e3e1f72630e28bd37d1b778be8430088c1a49b4
Vinicius Barcelos <josevsaraiva@gmail.com>
2020-11-23 11:51:16 -0300

    refact: separando a logica de storage em um provider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,6 @@
 import { container } from 'tsyringe';
 
+import '@shared/container/providers';
 import '@modules/users/providers';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
