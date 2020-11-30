b2a06c25e27d8bfbc24f9d8badaba73f074ffd50
Vinicius Barcelos <josevsaraiva@gmail.com>
2020-11-30 11:40:34 -0300

    feat: enviar email com token de reset de senha

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 app.use(
