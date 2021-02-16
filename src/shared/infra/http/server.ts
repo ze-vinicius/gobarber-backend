0c21ff91ff007472eac43177d4b71e9dc3d6ff53
Vinicius Barcelos <josevsaraiva@gmail.com>
2021-02-16 16:07:40 -0300

    api corrections

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -17,10 +17,10 @@ import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
 
-app.use(express.json());
-app.use(rateLimiter);
 app.use(cors());
+app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(rateLimiter);
 app.use(routes);
 
 app.use(errors());
